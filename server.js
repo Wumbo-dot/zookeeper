54dd523166ab8528f68bce7848d66fe224f7bd8f
Wumbo-dot <94250514+Wumbo-dot@users.noreply.github.com>
2022-07-13 17:07:25 -0500

    added animals and zookeeper pages

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-function filterByQuery(query, animalsArray) {
+function filterByQuery(query, animalsArray) { 
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
   if (query.personalityTraits) {
@@ -97,9 +96,22 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
